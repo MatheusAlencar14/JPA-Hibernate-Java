61076fcbc2b15e3755c87fa9cd0d418f7e4a2658
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-14 11:14:37 -0300

    Buscando dados no BD com JPA

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -9,19 +9,16 @@ import entities.Pessoa;
 public class Program {
 
     public static void main(String[] args) {
-
-        Pessoa p1 = new Pessoa(null, "Matheus", "matheus@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Miguel", "miguel@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Társilla", "tarsilla@gmail.com");
         
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
         
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 1);
+        
+        System.out.println(p);
+        
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
     }
 }
